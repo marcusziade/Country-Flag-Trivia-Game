43b7303030981b1b145a67dc33d65004898119a2
Marcus Ziadé <guitaripod@icloud.com>
2019-10-13 08:23:24 +0300

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Marcus Ziadé on 13.10.2019.
+//  Copyright © 2019 Marcus Ziadé. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
