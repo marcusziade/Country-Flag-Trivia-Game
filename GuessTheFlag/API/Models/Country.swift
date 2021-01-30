b1a7289d9a6ae025f39c8acc9da8aa7ac7eae3a5
marcusziade <guitaripod@icloud.com>
2021-01-30 19:51:33 +0200

    Expand Country model

--- GuessTheFlag/API/Models/Country.swift
+++ GuessTheFlag/API/Models/Country.swift
@@ -13,5 +13,25 @@ struct Country: Codable, Hashable {
     // MARK: - Properties
     let name: String
     let capital: String
+    let subregion: String
+    let population: Int
+    let latlng: [Double]
+    let demonym: String
+    let timezones: String
+    let borders: [String]
+    let activeName: String
     let flag: URL
+    let currencies: [Currency]
+    let languages: [Language]
+}
+
+struct Currency: Codable, Hashable {
+    let code: String
+    let name: String
+    let symbol: String
+}
+
+struct Language: Codable, Hashable {
+    let name: String
+    let nativeName: String
 }
