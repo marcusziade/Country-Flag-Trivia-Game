6a45578f2645aa9051867ab86613ec631461f90d
marcusziade <guitaripod@icloud.com>
2021-04-24 10:07:57 +0300

    Reformat UIView extensions

--- GuessTheFlag/Extensions/UIViewExtensions.swift
+++ GuessTheFlag/Extensions/UIViewExtensions.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 
+// MARK: - For autolayout
+
 extension UIView {
     
     func forAutoLayout() -> Self {
         self.translatesAutoresizingMaskIntoConstraints =  false
         return self
     }
+}
+
+// MARK: - Add shadow
+
+extension UIView {
 
     func addShadow(color: UIColor = .black, offset: CGSize = CGSize(width: 2.0, height: 2.0), opacity: Float = 0.3, radius: CGFloat = 4.0) {
         layer.shadowColor = color.cgColor
@@ -22,5 +29,23 @@ extension UIView {
         layer.shadowRadius = radius
         layer.masksToBounds = false
     }
+}
 
+// MARK: - Property Animator
+
+extension UIView {
+
+    // Fades a view in linearly with a given duration
+    func fadeIn(duration: TimeInterval) {
+        UIViewPropertyAnimator(duration: duration, curve: .linear) {
+            self.alpha = 1
+        }.startAnimation()
+    }
+
+    // Fades a view out linearly with a given duration
+    func fadeOut(duration: TimeInterval) {
+        UIViewPropertyAnimator(duration: duration, curve: .linear) {
+            self.alpha = 0
+        }.startAnimation()
+    }
 }
