9b79ffd0ca56a5bc72b9eb1048c95fd7313eb31f
marcusziade <guitaripod@icloud.com>
2021-04-24 10:08:12 +0300

    Add loadingSpinner to CountriesList

--- GuessTheFlag/View Controllers/CountriesListViewController.swift
+++ GuessTheFlag/View Controllers/CountriesListViewController.swift
@@ -12,30 +12,30 @@ import Combine
 class CountriesListViewController: UIViewController {
 
     // MARK: - Types
-    enum Section: CaseIterable {
+    private enum Section: CaseIterable {
         case countries
     }
     
     // MARK: - Properties
-    var imageCancellables = Set<AnyCancellable>()
-    var cancellables = Set<AnyCancellable>()
-    var countries: [Country] = [] {
+    private var imageCancellables = Set<AnyCancellable>()
+    private var cancellables = Set<AnyCancellable>()
+    private var countries: [Country] = [] {
         didSet {
             reload()
         }
     }
-    var dataSource: UICollectionViewDiffableDataSource<Section, Country>! = nil
-    let api = API()
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Country>! = nil
+    private let api = API()
     
     // MARK: - UI Components
-    lazy private var collectionView: UICollectionView = {
+    private lazy var collectionView: UICollectionView = {
         let view = UICollectionView(frame: .zero, collectionViewLayout: createLayout()).forAutoLayout()
         view.delegate = self
         view.backgroundColor = .clear
         return view
     }()
 
-    lazy var regionPicker: UISegmentedControl = {
+    private lazy var regionPicker: UISegmentedControl = {
         let picker = UISegmentedControl(type: Region.self)
         picker.selectedSegmentIndex = 0
         picker.addTarget(self, action: #selector(regionChanged), for: .valueChanged)
@@ -51,17 +51,29 @@ class CountriesListViewController: UIViewController {
         gradient.opacity = 0.4
         return gradient
     }()
+
+    private let loadingSpinner: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView().forAutoLayout()
+        view.startAnimating()
+        view.hidesWhenStopped = true
+        return view
+    }()
     
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.alpha = 0
         navigationItem.titleView = regionPicker
         navigationItem.backButtonDisplayMode = .minimal
         view.backgroundColor = .systemBackground
 
         view.layer.addSublayer(gradientLayer)
         view.addSubview(collectionView)
+        view.addSubview(loadingSpinner)
         NSLayoutConstraint.activate([
+            loadingSpinner.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loadingSpinner.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+
             collectionView.topAnchor.constraint(equalTo: view.topAnchor),
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
@@ -72,7 +84,7 @@ class CountriesListViewController: UIViewController {
     }
     
     // MARK: - Methods
-    func createLayout() -> UICollectionViewLayout {
+    private func createLayout() -> UICollectionViewLayout {
         let layout = UICollectionViewCompositionalLayout { [weak self] _, environment in
             let columns = self?.columnsFor(traitCollection: environment.traitCollection)
             let itemDimen: CGFloat = 1.0 / columns!
@@ -97,7 +109,7 @@ class CountriesListViewController: UIViewController {
         return layout
     }
 
-    func columnsFor(traitCollection: UITraitCollection) -> CGFloat {
+    private func columnsFor(traitCollection: UITraitCollection) -> CGFloat {
         switch (traitCollection.horizontalSizeClass, traitCollection.verticalSizeClass) {
             case (.compact, .regular): //iPhone Portrait
                 return 2
@@ -110,7 +122,7 @@ class CountriesListViewController: UIViewController {
         }
     }
 
-    func configureDataSource() {
+    private func configureDataSource() {
         dataSource = nil
         let cellRegistration = UICollectionView.CellRegistration<CountryCell, Country> { cell, indexPath, country in
             cell.configure(with: country)
@@ -126,14 +138,17 @@ class CountriesListViewController: UIViewController {
         reload()
     }
 
-    func reload() {
+    private func reload() {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Country>()
         snapshot.appendSections([.countries])
         snapshot.appendItems(countries)
         dataSource.apply(snapshot)
+
+        loadingSpinner.stopAnimating()
+        collectionView.fadeIn(duration: 0.25)
     }
 
-    func loadCountries(for region: Region = .europe) {
+    private func loadCountries(for region: Region = .europe) {
         let failing = Set<URL>([
             URL(string: "https://restcountries.eu/data/ecu.svg")!,
             URL(string: "https://restcountries.eu/data/nic.svg")!,
@@ -156,7 +171,7 @@ class CountriesListViewController: UIViewController {
     }
 
     // MARK: - Selectors
-    @objc func regionChanged() {
+    @objc private func regionChanged() {
         HapticEngine.select.selectionChanged()
         switch regionPicker.selectedSegmentIndex {
             case 0:
