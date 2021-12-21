69d8e86b92093d5e6fd2d2a36a717dcf08131da4
Marcus Ziadé <marcus.ziade@ouraring.com>
2021-12-21 10:34:02 +0200

    Disable ignoresafearea bottom for map

--- GuessTheFlag/Encyclopedia/SwiftUI/Screens/CountryDetail.swift
+++ GuessTheFlag/Encyclopedia/SwiftUI/Screens/CountryDetail.swift
@@ -54,7 +54,7 @@ struct CountryDetail: View {
                 
                 Section(header: Text("Maps")) {
                     NavigationLink(
-                        destination: Map(coordinateRegion: $viewModel.region).edgesIgnoringSafeArea(.all)) {
+                        destination: Map(coordinateRegion: $viewModel.region).edgesIgnoringSafeArea(.top)) {
                             Label("Apple Maps", systemImage: "map")
                         }
                     Link(destination: country.googleMap) {
