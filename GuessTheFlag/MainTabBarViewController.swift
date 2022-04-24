64f4ebca78042901ae3bef63d2b63eb7a9933980
Marcus Ziadé <marcus.ziade@ouraring.com>
2022-04-24 18:35:56 +0300

    Set default tab to game

--- GuessTheFlag/MainTabBarViewController.swift
+++ GuessTheFlag/MainTabBarViewController.swift
@@ -28,7 +28,7 @@ final class MainTabBarController: UITabBarController {
             countriesViewController,
             settingsViewController
         ]
-        selectedIndex = 2
+        selectedIndex = 0
     }
     
     // MARK: - Private
