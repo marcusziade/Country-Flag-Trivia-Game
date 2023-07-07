d52b5d1f0f3ba5d8a8f7c2e1e226e35d469586b0
marcusziade <marcus.ziade@ouraring.com>
2023-07-07 03:54:26 +0300

    Refactor CountryDetail view to improve code readability and organization

--- master-of-flags/Encyclopedia/SwiftUI/Screens/CountryDetail.swift
+++ master-of-flags/Encyclopedia/SwiftUI/Screens/CountryDetail.swift
@@ -2,76 +2,94 @@ import MapKit
 import SwiftUI
 
 struct CountryDetail: View {
-
+    
     @StateObject var viewModel: CountryDetailVM
-
+    
     let country: Country
-
+    
     var body: some View {
-        VStack {
-            AsyncImage(url: country.flag) {
-                $0.image?
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 250)
+        ScrollView {
+            VStack(alignment: .leading) {
+                AsyncImage(url: country.flag) {
+                    $0.image?
+                        .resizable()
+                        .frame(height: 250)
+                }
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text(country.name.official).font(.title).fontWeight(.bold).padding(.top)
+                    
+                    DetailSection(header: "Capital") { Text(country.capitalCity) }
+                    DetailSection(header: "Capital") { Text(country.capitalCity) }
+                    DetailSection(header: "Region") { Text(country.region) }
+                    DetailSection(header: "Population") { Text("\(country.population)") }
+                    
+                    DetailSection(header: "Currencies") {
+                        ForEach(country.countryCurrencies, id: \.self) { Text($0) }
+                    }
+                    
+                    DetailSection(header: "Languages") {
+                        ForEach(country.countryLanguages, id: \.self) { Text($0) }
+                    }
+                    
+                    if let url = URL(string: "https://en.wikipedia.org/wiki/\(country.name.common)") {
+                        Link(destination: url) {
+                            Text("Study on Wikipedia")
+                                .font(.headline)
+                                .foregroundColor(.blue)
+                        }
+                        .padding(.vertical)
+                    }
+                    
+                    MapsSection(country: country, viewModel: viewModel)
+                }
+                .padding(.horizontal)
+                Spacer()
             }
-
-            Form {
-                Text(country.name.official).font(.title)
-                Section(header: Text("Capital")) {
-                    Text(country.capitalCity)
-                }
-
-                Section(header: Text("Region")) {
-                    Text(country.region)
-                }
-
-                Section(header: Text("Currencies")) {
-                    ForEach(country.countryCurrencies, id: \.self) { Text($0) }
-                }
-
-                Section(header: Text("Population")) {
-                    Text("\(country.population)")
-                }
-
-                Section(header: Text("Languages")) {
-                    ForEach(country.countryLanguages, id: \.self) { Text($0) }
-                }
-
-                if let url = URL(string: "https://en.wikipedia.org/wiki/\(country.name.common)") {
-                    Link(destination: url) {
-                        Text("Study on Wikipedia")
-                            .foregroundColor(.blue)
-                    }
-                }
-
-                Section(header: Text("Maps")) {
-                    NavigationLink(
-                        destination: Map(coordinateRegion: $viewModel.region).edgesIgnoringSafeArea(.all)
-                    ) {
-                        Label("Apple Maps", systemImage: "map")
-                    }
-                    Link(destination: country.googleMap) {
-                        Label("Google Maps", systemImage: "map")
-                    }
-                    Link(destination: country.openStreetMap) {
-                        Label("Open Street Map", systemImage: "map")
-                    }
-                }
-                .foregroundColor(.blue)
-            }
-            Spacer()
         }
         .edgesIgnoringSafeArea(.top)
     }
 }
 
-struct CountryDetail_Previews: PreviewProvider {
+struct DetailSection<Content: View>: View {
+    let header: String
+    let content: Content
+    
+    init(header: String, @ViewBuilder content: () -> Content) {
+        self.header = header
+        self.content = content()
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(header).font(.headline).foregroundColor(.accentColor)
+            content
+        }
+    }
+}
 
-    static var previews: some View {
-        CountryDetail(
-            viewModel: CountryDetailVM(coordinate: CLLocationCoordinate2D(latitude: 60.1699, longitude: 24.9384)),
-            country: Country.mockCountry
-        )
+struct MapsSection: View {
+    let country: Country
+    @ObservedObject var viewModel: CountryDetailVM
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Maps").font(.headline).foregroundColor(.accentColor)
+            NavigationLink(destination: Map(coordinateRegion: $viewModel.region).edgesIgnoringSafeArea(.all)) {
+                Label("Apple Maps", systemImage: "map")
+                    .font(.headline)
+                    .foregroundColor(.primary)
+            }
+            Link(destination: country.googleMap) {
+                Label("Google Maps", systemImage: "map")
+                    .font(.headline)
+                    .foregroundColor(.primary)
+            }
+            Link(destination: country.openStreetMap) {
+                Label("Open Street Map", systemImage: "map")
+                    .font(.headline)
+                    .foregroundColor(.primary)
+            }
+        }
     }
 }
